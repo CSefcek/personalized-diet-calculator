5f502df3257881dddd8e3dc797915c74dc058690
CSefcek <carlosefcek@gmail.com>
2025-07-31 23:31:22 +0200

    Store user input in variables

---
+++ script.js
@@ -0,0 +1,7 @@
+let gender = prompt("What's your gender?");
+let age = prompt("How old are you?");
+let height = prompt("What's your height in cm?");
+let weight = prompt("What's your weight in kg?");
+let physicalActivity = prompt("What's your level of physical activity (sedentary/light/moderate/heavy)?");
+let foodPreferenceRestriction = prompt("Do you have any food preference or restriction?");
+
